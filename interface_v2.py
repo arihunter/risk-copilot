33b8a52a1b4e438ff6709b8c25ca37a5cd04b611
Ritika Agarwal <agarwalritika101@gmail.com>
2023-09-16 22:47:55 +0530

    fixesss

--- interface_v2.py
+++ interface_v2.py
@@ -84,7 +84,7 @@ if "query" not in st.session_state.keys():
   st.session_state.query = ""
 
 
-dataset_keys = ["lms","credit-decisioning","location"]
+dataset_keys = ["lms","credit-decisioning","collection","location"]
 for key in dataset_keys:
   if key not in st.session_state :
     st.session_state[key] = False
@@ -106,22 +106,21 @@ def gpt_helper(query:str,context:str) -> str:
   )
   return response["choices"][0]["message"]["content"]
 
-output_formatting_prompt = f"""I need you to answer the user's query using the given context. The response to the query is certain to be in the context. Go carefully through the query and context and just return the answer, nothing else. Dont make anything up. Dont do any calculations on your end. Do not assume any denomination for the requested metrics in the query. Now using the given context answer the query. Context: \n"""
+# context = ""
+# output_formatting_prompt = f"""I need you to answer the user's query using the given context. The response to the query is certain to be in the context. Go carefully through the query and context and just return the answer, nothing else. Dont make anything up. Dont do any calculations on your end. Do not assume any denomination for the requested metrics in the query. Now using the given context answer the query. Context: 
+# {str(context)}"""
+
 
 # #====reading all my datasets========
-credit_decisioning_df = pd.read_csv("credit-decisioning_data.csv")
-location_df = pd.read_csv("location_data.csv")
-master_df_dict = {"bureau_data" : credit_decisioning_df, "location_data" : location_df}
-master_col_dict = {"bureau_data" : credit_decisioning_df.columns, "location_data" : location_df.columns}
 
-def pick_data_set(prompt : str)-> str:
+def pick_data_set(prompt : str , master_col_dict : dict)-> str:
   pick_data_set_prompt = """
   I will provide you with a user query, you have to analyse the query carefully and identify the feature name mentioned in the query. 
   If you're able to identify a feature from the user query you have to respond with the feature name else NO.
   Here are some examples for you.
 
   Example 1:
-  Query: do the risk profiling of my portfolio using the bureau score
+  Query: do the risk profiling of my portfolio using the bureau score for financial year 2021
   Response: bureau score
 
   Example 2:
@@ -136,7 +135,7 @@ def pick_data_set(prompt : str)-> str:
   if col_name_extracted == "NO":
     return "There was insufficent information to do the analysis. Ask the user to give feature name in the query. Do not make up any random metrics by yourself"
   for key in master_col_dict:
-    col_list = master_col_dict[key]
+    col_list = master_col_dict['key']
     for col in col_list:
       score = SequenceMatcher(None, col_name_extracted, col).ratio()
       if(score > 0.7):
@@ -186,7 +185,7 @@ def calculate_risk_metrics(start_dt:str,end_dt:str) -> float:
     return "There was insufficent date information to do the calculations. Ask the user to give complete date information in the query. Do not make up any random metrics by yourself."
 
   lms_df = pd.read_csv("lms_data.csv")
-  lms_df["due_date"] = lms_df["due_date"].apply(lambda x: dparser.parse(x.split(" ")[0], dayfirst=True))
+  lms_df["due_date"] = lms_df["due_date"].apply(lambda x: dparser.parse(x, dayfirst=True))
   lms_df_filtered = lms_df[(lms_df['due_date']) >= dparser.parse(start_dt, dayfirst=False)]
   lms_df_filtered = lms_df_filtered[(lms_df_filtered['due_date']) <= dparser.parse(end_dt, dayfirst=False)]
   lms_df_filtered["defaulted_amount"] = lms_df_filtered["is_default"] * lms_df_filtered["loan_amount"]
@@ -220,8 +219,6 @@ def calculate_risk_metrics(start_dt:str,end_dt:str) -> float:
   Context:
   {str(context)}
   """
-  # responsePrompt = output_formatting_prompt
-  # responsePrompt += str(context)
   capture_message(f"The values calculated are {context}")
   response = gpt_helper(prompt,responsePrompt)
   capture_message(f"Gpt helper response for risk metric function output formatting {response}")
@@ -237,7 +234,6 @@ def calculate_bureau_metrics(start_dt:str,end_dt:str) -> str:
       avg_bureau_non_defaulters is the average bureau scores of non-defaulters
       avg_ticket_siZe_defaulters is the average loan ticket siZe of defaulters
       avg_ticket_siZe_non_defaulters is the average loan ticket siZe of non-defaulters"""
-  
   dataset_idx=[0,1]
   for idx in dataset_idx:
     if st.session_state[dataset_keys[idx]] == False:
@@ -245,7 +241,7 @@ def calculate_bureau_metrics(start_dt:str,end_dt:str) -> str:
   
   lms_df = pd.read_csv("lms_data.csv")
   credit_decisioning_df = pd.read_csv("credit-decisioning_data.csv")
-  lms_df["due_date"] = lms_df["due_date"].apply(lambda x: dparser.parse(x.split(" ")[0], dayfirst=True))
+  lms_df["due_date"] = lms_df["due_date"].apply(lambda x: dparser.parse(x, dayfirst=True))
   lms_df_filtered = lms_df[(lms_df['due_date']) >= dparser.parse(start_dt, dayfirst=False)]
   lms_df_filtered = lms_df_filtered[(lms_df_filtered['due_date']) <= dparser.parse(end_dt, dayfirst=False)]
   lms_df_filtered["defaulted_amount"] = lms_df_filtered["is_default"] * lms_df_filtered["loan_amount"]
@@ -258,14 +254,35 @@ def calculate_bureau_metrics(start_dt:str,end_dt:str) -> str:
   total = credit_decisioning_lms_df['user_id'].nunique()
   non_ntc_count = total - ntc_count
   ntc_npa = ntc_df['defaulted_amount'].sum()/ntc_df['loan_amount'].sum()
-  non_ntc_npa = non_ntc_df['defaulted_amount'].sum()/non_ntc_df['loan_amount'].sum()
+  non_ntc_npa = ntc_df['defaulted_amount'].sum()/ntc_df['loan_amount'].sum()
   avg_bureau_defaulters = credit_decisioning_lms_df[credit_decisioning_lms_df['is_default'] == 1][bureau_score_col].mean()
   avg_bureau_non_defaulters = credit_decisioning_lms_df[credit_decisioning_lms_df['is_default'] == 0][bureau_score_col].mean()
-  avg_ticket_siZe_defaulters = lms_df[lms_df['is_default'] == 1]['loan_amount'].mean()
-  avg_ticket_siZe_non_defaulters = lms_df[lms_df['is_default'] == 0]['loan_amount'].mean()
+  avg_ticket_siZe_defaulters = lms_df[lms_df['is_default'] == "1"].mean()
+  avg_ticket_siZe_non_defaulters = lms_df[lms_df['is_default'] == "0"].mean()
   context = {'ntc_count' : ntc_count, 'non_ntc_count' : non_ntc_count, 'ntc_npa' : ntc_npa,'non_ntc_npa' : non_ntc_npa, 'total_users' : total, 'avg_bureau_defaulters' : avg_bureau_defaulters, 'avg_bureau_non_defaulters' : avg_bureau_non_defaulters, 'avg_ticket_siZe_defaulters': avg_ticket_siZe_defaulters, 'avg_ticket_siZe_non_defaulters' : avg_ticket_siZe_non_defaulters}
-  responsePrompt = output_formatting_prompt 
-  responsePrompt += "\n" + str(context)
+  example_context1 = {'ntc_count' : 100, 'non_ntc_count' : 200, 'ntc_npa' : 0.12,'non_ntc_npa' : 0.04, 'total_users' : 300, 'avg_bureau_defaulters' : 500, 'avg_bureau_non_defaulters' : 750, 'avg_ticket_siZe_defaulters': 1000, 'avg_ticket_siZe_non_defaulters' : 1500}
+  example_context2 = {'ntc_count' : 1000, 'non_ntc_count' : 2000, 'ntc_npa' : 0.22,'non_ntc_npa' : 0.14, 'total_users' : 3000, 'avg_bureau_defaulters' : 550, 'avg_bureau_non_defaulters' : 725, 'avg_ticket_siZe_defaulters': 1200, 'avg_ticket_siZe_non_defaulters' : 1700}
+  responsePrompt = f"""
+  I need you to answer the users query using the given context. 
+  The response to the query is certain to be in the context.
+  Go carefully through the query and context and just return the answer, nothing else.
+  Dont make anything up. Dont do any calculations on your end. Do not assume any denomination for the requested metrics in the query. 
+  Here are some examples for you.
+
+  Example 1:
+  Context : {example_context1}
+  Query: "Calculate the NTC count for third financial quarter of 2021"
+  Response: The NTC count for the third financial quarter of 2021 is 1000.
+
+  Example 2:
+  Context : {example_context2}
+  Query : "Calculate the average bureau score of non-defaulters for first financial quarter of 2022"
+  Response: The average bureau score of non-defaulters for first financial quarter of 2022 is 725
+
+  Now using the given context answer the query.
+  Context:
+  {str(context)}
+  """
   capture_message(f"The values calculated are {context}")
   response = gpt_helper(prompt,responsePrompt)
   capture_message(f"Gpt helper response for risk metric function output formatting {response}")
@@ -284,29 +301,33 @@ def risk_profiling(start_dt:str,end_dt:str) -> str:
   """
   # start_dt = st.text_input("Start Date")
   # end_dt = st.text_input("End Date")
-  dataset_idx=[0,1]
+
+  dataset_idx=[0, 1, 3]
   for idx in dataset_idx:
     if st.session_state[dataset_keys[idx]] == False:
       return "Sufficient data not available !, please provide all the required data."
+  lms_df = pd.read_csv("lms_data.csv")
+  credit_decisioning_df = pd.read_csv("credit-decisioning_data.csv")
+  location_df = pd.read_csv("location_data.csv")
+  master_df_dict = {"bureau_data" : credit_decisioning_df, "location_data" : location_df}
+  master_col_dict = {"bureau_data" : credit_decisioning_df.columns, "location_data" : location_df.columns}
+  #picking the dataset given the prompt=========
+  dataset_name, col_name = pick_data_set(prompt, master_col_dict)
   #sanity check
   dateCheckPrompt = "I will provide you with a user query, you have to analyse the query carefully and identify if there is a time period mentioned in the query. You have to respond only with YES or NO depending on the query."
   dateCheck = gpt_helper(prompt,dateCheckPrompt)
   if dateCheck == "NO":
     return "There was insufficent date information to do the calculations. Ask the user to give complete date information in the query. Do not make up any random metrics by yourself."
-  lms_df = pd.read_csv("lms_data.csv")
-  #===picking the dataset given the prompt=========
-  dataset_name, col_name = pick_data_set(prompt)
-  dataset_df = master_df_dict[dataset_name] #======how to handle this @Arihant??======
-  credit_decisioning_df = pd.read_csv("credit-decisioning_data.csv")
-  lms_df["due_date"] = lms_df["due_date"].apply(lambda x: dparser.parse(x.split(" ")[0], dayfirst=True))
+  dataset_df = master_df_dict[dataset_name]
+  lms_df["due_date"] = lms_df["due_date"].apply(lambda x: dparser.parse(x, dayfirst=True))
   lms_df_filtered = lms_df[(lms_df['due_date']) >= dparser.parse(start_dt, dayfirst=False)]
   lms_df_filtered = lms_df_filtered[(lms_df_filtered['due_date']) <= dparser.parse(end_dt, dayfirst=False)]
   lms_df_filtered["defaulted_amount"] = lms_df_filtered["is_default"] * lms_df_filtered["loan_amount"]
-  credit_decisioning_df = credit_decisioning_df.drop_duplicates()
-  credit_decisioning_lms_df = pd.merge(lms_df_filtered, credit_decisioning_df, left_on = ["user_id"], right_on = ["user_id"], how = "left")
-  credit_decisioning_lms_df = bin_df(credit_decisioning_lms_df, col_name, 5)
+  dataset_df = dataset_df.drop_duplicates()
+  dataset_lms_df = pd.merge(lms_df_filtered, dataset_df, left_on = ["user_id"], right_on = ["user_id"], how = "left")
+  dataset_lms_df = bin_df(dataset_lms_df, col_name, 5)
   col_group_name = col_name + "_groups"
-  grouped_df = credit_decisioning_lms_df.groupby(col_group_name, dropna = False).agg({'user_id' : 'count','defaulted_amount' : 'sum', 'loan_amount' : 'sum'}).reset_index()
+  grouped_df = dataset_lms_df.groupby(col_group_name, dropna = False).agg({'user_id' : 'count','defaulted_amount' : 'sum', 'loan_amount' : 'sum'}).reset_index()
   grouped_df["npa"] = grouped_df["defaulted_amount"]/ grouped_df["loan_amount"]
   grouped_df["fraction_of_users"] = grouped_df["user_id"]/grouped_df["user_id"].sum()
   return grouped_df.to_string()
@@ -316,7 +337,7 @@ def calculate_top_features(external_data_lms_df, labels, num_fts = 10):
   gbm = ensemble.GradientBoostingRegressor()
   gbm.fit(features,labels)
   feature_importance = gbm.feature_importances_
-  score_with_idx = [] 
+  score_with_idx = []
   for idx,score in enumerate(feature_importance):
     score_with_idx.append((score,idx))
   sortedFeatures = sorted(score_with_idx)
@@ -324,12 +345,10 @@ def calculate_top_features(external_data_lms_df, labels, num_fts = 10):
   #===presently taking top 10 features only=====
   sortedFeatures = sortedFeatures[:num_fts]
   sortedIdx = [x[1] for x in sortedFeatures]
-  top_fts = [features.columns[x[1]] for x in sortedFeatures]
-  # top_fts = [features.columns[x] for x in sortedIdx]
-  # df_with_top_fts = features.iloc[:,sortedIdx]
-  # top_fts = list(df_with_top_fts.columns)
-  # print(top_fts_v1)
-  return str(top_fts)
+  #top_fts = [features.columns[x] for x in sortedIdx]
+  df_with_top_fts = features.iloc[:,sortedIdx]
+  top_fts = list(df_with_top_fts.columns)
+  return top_fts
 
 def evaluate_data_source():
   """
@@ -338,14 +357,14 @@ def evaluate_data_source():
   Here top_fts has a list of top 10 features.
   """
   #hardcoding external data source here @Arihant - please make the change for the user to i/p the data source
-  dataset_idx=[2]
+  dataset_idx=[3]
   for idx in dataset_idx:
     if st.session_state[dataset_keys[idx]] == False:
       return "Sufficient data not available !, please provide all the required data."
   external_data = pd.read_csv("location_data.csv")
   external_data.dropna(inplace=True)
   labels = external_data["dep_var"]
-  external_data.drop(["dep_var","address", "installments_due_latest_y"],axis=1,inplace=True)
+  external_data.drop(["dep_var","address"],axis=1,inplace=True)
   top_fts = calculate_top_features(external_data,labels)
   #here both top_fts and response are to be processed wrt to a gpt_helper function. Here we might also need to output graphical trends
   return top_fts
@@ -372,67 +391,27 @@ def UploadedFileCallback(displayText:str):
   print(displayText)
 
 with st.sidebar:
-
-  #lms data
-  LMS_DATA = st.file_uploader("LMS_DATA",type="csv",key="data_1")
-  if LMS_DATA is not None:
-    lms_path = "lms_data.csv"
-    with st.spinner("Uploading"):
-      st.session_state["lms"] = True
-      pd.read_csv(LMS_DATA).to_csv(lms_path)
-
-
-  CREDIT_DECISIONING_DATA = st.file_uploader("BUREAU_DATA",type="csv",key="data_2")
-  if CREDIT_DECISIONING_DATA is not None:
-    bureau_path = "credit-decisioning_data.csv"
-    with st.spinner("Uploading"):
-      st.session_state["credit-decisioning"] = True
-      pd.read_csv(CREDIT_DECISIONING_DATA).to_csv(bureau_path)
-
-
-
-  # COLLECTION_DATA = st.file_uploader("COLLECTION_DATA",type="csv",key="data_3")
-  # if COLLECTION_DATA is not None:
-  #   collection_path = "collection_data.csv"
-  #   with st.spinner("Uploading"):
-  #     st.session_state["collection"] = True
-  #     pd.read_csv(COLLECTION_DATA).to_csv(collection_path)
-
-
-
-  LOCATION_DATA = st.file_uploader("LOCATION_DATA",type="csv",key="data_4")
-  if LOCATION_DATA is not None:
-    location_path = "location_data.csv"
-    with st.spinner("Uploading"):
-      st.session_state["location"] = True
-      pd.read_csv(LOCATION_DATA).to_csv(location_path)
-
-
-
-
-
-  # DatasetOption = st.selectbox("Choose the Dataset",("LMS","Credit-Decisioning","Collection","Location"))
+  DatasetOption = st.selectbox("Choose the Dataset",("LMS","Credit-Decisioning","Collection","Location"))
   
-  
-  # #Data Upload
-  # if st.session_state[str(DatasetOption.lower())] == True:
-  #   st.warning("Uploading now will update the dataset")
-  #   displayText = "Updating the " + DatasetOption + "dataset"
-  #   UploadedFile = st.file_uploader("Update the file here",type="csv",on_change=UploadedFileCallback,args=[displayText])
-  #   if UploadedFile is not None:
-  #     dataset_path = str(DatasetOption.lower()) + "_data.csv"
-  #     string_path = str(DatasetOption.lower()) + "_data.txt"
-  #     with st.spinner("Updating"):
-  #       pd.read_csv(UploadedFile).to_csv(dataset_path)
-  # else:
-  #   displayText = "Uploading the " + DatasetOption + "dataset"
-  #   UploadedFile = st.file_uploader("Upload the file here",type="csv",on_change=UploadedFileCallback,args=[displayText])
-  #   if UploadedFile is not None:
-  #     st.session_state[str(DatasetOption.lower())] = True
-  #     dataset_path = str(DatasetOption.lower()) + "_data.csv"
-  #     string_path = str(DatasetOption.lower()) + "_data.txt"
-  #     with st.spinner("Uploading"):
-  #       pd.read_csv(UploadedFile).to_csv(dataset_path)
+  #Data Upload
+  if st.session_state[str(DatasetOption.lower())] == True:
+    st.warning("Uploading now will update the dataset")
+    displayText = "Updating the " + DatasetOption + "dataset"
+    UploadedFile = st.file_uploader("Update the file here",type="csv",on_change=UploadedFileCallback,args=[displayText])
+    if UploadedFile is not None:
+      dataset_path = str(DatasetOption.lower()) + "_data.csv"
+      string_path = str(DatasetOption.lower()) + "_data.txt"
+      with st.spinner("Updating"):
+        pd.read_csv(UploadedFile).to_csv(dataset_path)
+  else:
+    displayText = "Uploading the " + DatasetOption + "dataset"
+    UploadedFile = st.file_uploader("Upload the file here",type="csv",on_change=UploadedFileCallback,args=[displayText])
+    if UploadedFile is not None:
+      st.session_state[str(DatasetOption.lower())] = True
+      dataset_path = str(DatasetOption.lower()) + "_data.csv"
+      string_path = str(DatasetOption.lower()) + "_data.txt"
+      with st.spinner("Uploading"):
+        pd.read_csv(UploadedFile).to_csv(dataset_path)
 
 
 userEmail = st.experimental_user.email
@@ -466,6 +445,8 @@ def ClearCallback():
   st.session_state.thumbs = True
 
 
+
+
 @st.cache_resource(show_spinner=False)
 def get_response(query:str) -> str:
   response = agentLlama.chat(query)
@@ -482,13 +463,11 @@ with col3:
   
 st.markdown("")
 if st.session_state.generate:
-	ResponseToDisplay = ""
 	with st.status("Generating your response") as status:
 		response = get_response(str(prompt))
 		status.update(label="Done",state="complete")
 	placeholder = st.empty()
-	ResponseToDisplay = response
-	st.write(f'<font size="4">{ResponseToDisplay}</i>',unsafe_allow_html=True)
+	st.write(f'<font size="4">{response}</i>',unsafe_allow_html=True)
 	with placeholder.container():
 		relevantCol1,relevantCol2,relevantCol3 = st.columns([0.8,0.1,0.1])
 		if (st.session_state.thumbs == False):
